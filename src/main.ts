6bce5d12cd4310ebe9fb1bb8c067ec6e933c5cc7
Juan Dominguez <juanignaciodom3@gmail.com>
2026-05-14 19:34:52 -0300

    add /api/recipes prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/recipes');
   await app.listen(process.env.PORT ?? 3001);
 }
 
